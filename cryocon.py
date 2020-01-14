284a9f7a899c6ed0895d00888be9aba15e2d43ec
Cody <carobers@asu.edu>
2020-01-14 14:41:08 -0700

    Added vscode settings and plot example

--- cryocon.py
+++ cryocon.py
@@ -6,20 +6,44 @@ values obtained from here will be made available upon request from the plotting 
 '''
 
 import serial
+import sys
+import os
 
 class Cryocon():
     def __init__(self):
-        try:
-            ser = serial.Serial("/dev/ttyUSB0", 9600, timeout=5)
-        except (OSError, serial.SerialException):
-            print("Serial Error, please ensure that the device is conected and the correct port is.")
-        
-        print("Init successfull")
+        # create a serial object
+        self.ser = serial.Serial()
 
+        # Configure and attempt to open the serial port
+        try:
+           self.ser.baudrate = 9600
+           self.ser.port = "/dev/ttyUSB0"
+           self.ser.open()
+
+        except (OSError, serial.SerialException):
+            print("[Cryocon] Serial Error, please ensure that the device is conected and the correct port is selected.")
+
+
+    def getTemperatures(self):
+        return [0,0]
+
+
+    def closeConnection(self):
+        if self.ser.is_open:
+            self.ser.close()
+            print("[Cryocon] Closed serial connection")
+        else:
+            print("[Cryocon] Serial Connection was already closed")
 
 def main():
-    print("Main Function for testing functionality in cryocon.pyd")
-    device = Cryocon()
+    if sys.version_info < (3,0):
+        print("Error, please use python 3")
+        exit()
+    
+    dev = Cryocon()
+    print(dev.getTemperatures())
+    dev.closeConnection()
+
 
 if __name__=="__main__":
     main()
